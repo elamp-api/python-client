f84957cb6e3b07fee9331460aa4f28ec070152c6
Florian Bojda <florian@elamp.fr>
2019-07-18 23:45:36 +0200

    improv : implement retry mecanism on api requests

--- elampclient/elamprequest.py
+++ elampclient/elamprequest.py
@@ -1,9 +1,21 @@
 import requests
 import json
 import six
+import random
+import time
 import sys
 import platform
 from .version import __version__
+
+import logging
+logging.basicConfig(level=logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s : %(message)s')
+handler.setFormatter(formatter)
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+
+_TOO_MANY_REQUESTS = 429
 
 
 class eLampRequest(object):
@@ -50,7 +62,26 @@ class eLampRequest(object):
             return False
         return True
 
-    def do(self, token, request="?", http_method='post', post_data=None, domain="api.elamp.fr", timeout=None):
+    def _should_retry_response(self, resp):
+        if not resp:
+            return True
+
+        # Retry on 5xx errors.
+        if resp.status_code >= 500:
+            return True
+
+        # Retry on 429 errors.
+        if resp.status_code == _TOO_MANY_REQUESTS:
+            return True
+
+        # Everything inside 200-299 status code is OK => should not be retried
+        if resp.status_code >= 200 and resp.status_code < 300:
+            return False
+
+        if resp.status_code >= 300:
+            return True
+
+    def do(self, token, request="?", http_method='post', post_data=None, domain="api.elamp.fr", timeout=None, retries=3):
         """
         Perform a POST request to the eLamp Web API
         Args:
@@ -93,12 +124,33 @@ class eLampRequest(object):
         #        post_data[k] = json.dumps(v)
 
         # Submit the request
-        print(http_method, url)
-        return requests.request(
-            http_method,
-            url,
-            headers=headers,
-            data=post_data,
-            timeout=timeout,
-            proxies=self.proxies
-        )
+        resp = None
+        for retry_num in range(retries + 1):
+            if retry_num > 0:
+                sleep_time = random.random() * 2 ** retry_num
+                logger.info('[elamp] request : retrying request, attempt {} after {} seconds'.format(retry_num, sleep_time))
+                time.sleep(sleep_time)
+            logger.info('[elamp] request : [{}] {}'.format(http_method, url))
+            exception = None
+            try:
+                resp = requests.request(
+                    http_method,
+                    url,
+                    headers=headers,
+                    data=post_data,
+                    timeout=timeout,
+                    proxies=self.proxies
+                )
+            except Exception as e:
+                exception = e
+
+            if exception:
+                if retry_num == retries:
+                    raise exception
+                else:
+                    continue
+
+            if not self._should_retry_response(resp):
+                break
+
+        return resp
--- tests/elampclient_test.py
+++ tests/elampclient_test.py
@@ -7,7 +7,7 @@ from elampclient import eLampClient
 class eLampClientTest(unittest.TestCase):
 
     def test_basic_client(self):
-        client = eLampClient('Bearer token')
+        client = eLampClient('TOKEN')
         #response = client.skills().get("SKI1", test='test') #.list(skill="SKI1")
         #response = client.powers().create(user='U1', skill='SKI1')
         response = client.api_call(method='applications/me/infos', http_method='get', domain='team.elamp.fr/admin/api')
